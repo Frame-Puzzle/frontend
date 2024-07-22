e22f8bd79af86508cbbac32ecd70e04df7dfeb86
홍경현 <hyeon21owo@gmail.com>
2024-07-22 17:01:13 +0900

    [FEAT][BE][경현] init

---
+++ backend/frazzle/src/main/java/com/frazzle/main/FrazzleApplication.java
@@ -0,0 +1,13 @@
+package com.frazzle.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrazzleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrazzleApplication.class, args);
+    }
+
+}
---
+++ backend/frazzle/src/test/java/com/frazzle/main/FrazzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frazzle.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrazzleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
