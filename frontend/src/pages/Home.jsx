f955820cab3d3b6a3142a8f71fe37675adeff157
ayoung-kim <ayoung7777@gmail.com>
2024-07-26 14:42:55 +0900

    [FIX][FE][아영] kakao 컴포넌트 오류

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,14 @@
+
 import MainNav from "../components/common/MainNav";
+import "./Home.css";
 
 const Home = () => {
   return (
+    // Page 단위의 Component의 최상위 요소에는 반드시 width: 100%와 height: 100% 속성이 필요하다.
     <div className="w-full h-full flex flex-wrap">
-      <div className="home-main-content"></div>
-      <div className="home-footer"><MainNav /></div>
+      <div className="home-footer">
+        <MainNav />
+      </div>
     </div>
   );
 };
