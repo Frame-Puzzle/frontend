49b9454ed10f328ecec1845574b8bff8d7a567a7
ayoung-kim <ayoung7777@gmail.com>
2024-07-26 10:49:47 +0900

    [ADD][FE][아영] home 컴포넌트 수정

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,8 @@
-import KakaoLoginButton from "../components/login/KakaoLoginButton";
 import MainNav from "../components/common/MainNav";
-import GoogleLoginButton from "../components/login/GoogleLoginButton";
-
 
 const Home = () => {
   return (
     <div className="w-full h-full">
-      <KakaoLoginButton />
-      <GoogleLoginButton />
       <footer>
         <MainNav />
       </footer>
