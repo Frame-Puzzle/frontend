65fcc2ca6fd3b8093cf8cd5960926ccdb167300e
THIP-Y <nine14141732@gmail.com>
2024-07-26 10:08:05 +0900

    [FEAT][FE][승찬] arrange mainframe

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,30 @@
 import "./App.css";
 import "./index.css";
-import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Notification from "./pages/Notification";
 import MyPage from "./pages/MyPage";
+import { useEffect } from "react";
 
 function App() {
+
+  const setScreenSize = () => {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+  }
+  
+  useEffect(() => {
+    setScreenSize();
+  });
+
   return (
-    <>
+    <div className="main-frame">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/notification" element={<Notification />} />
         <Route path="/mypage" element={<MyPage />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
