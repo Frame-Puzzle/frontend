1cbf39333f4870312e85c9756c9efe8bf28bae21
hyun-young <jhy7547@naver.com>
2024-07-28 15:57:08 +0900

    [FEAT][FE][현영] vite-plugin-pwa vite.config.js 초기 적용

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/ 참고
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Test PWA",
+        short_name: "TestPWA",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icons/figma192.png", // icons 폴더 내의 아이콘 임시 파일
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
