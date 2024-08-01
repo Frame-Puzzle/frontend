fb702f9f0c41d01e8eb8f77cd597310daa3301b4
ayoung-kim <ayoung7777@gmail.com>
2024-08-02 00:27:38 +0900

    [FEAT][FE][아영] 닉네임 중복 확인

--- frontend/src/pages/ChangeNickName.jsx
+++ frontend/src/pages/ChangeNickName.jsx
@@ -5,6 +5,7 @@ import MainNav from "../components/common/MainNav";
 import "./ChangeNickName.css";
 import { setNickName } from "../stores/userSlice";
 import userApi from "../apis/userApi";
+import { useNavigate } from "react-router-dom";
 
 // 1. 가운데 동그란 프로필 사진
 // 1-1. 동그란 사진 아래에 버튼 누르면 (사진 보관함 연결 버튼 보이게 클릭)
@@ -15,24 +16,45 @@ import userApi from "../apis/userApi";
 // 6. 아래에 로그아웃, 회원탈퇴 버튼 생성
 
 const ChangeNickName = () => {
+  const nav = useNavigate();
+
   const user = useSelector((state) => state.user);
   const dispatch = useDispatch();
+
   const [newNickName, setNewNickName] = useState("");
   const [isDuplicated, setIsDuplicated] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
 
+  const submitNewNickName = async () => {
+    const response = await userApi.put(`/nickname?nickname=${newNickName}`);
+    dispatch(setNickName(response.data.nickname));
+
+    nav("/mypage");
+  };
   const handleNickNameChange = (e) => {
     setNewNickName(e.target.value);
-    checkIsDuplicated(e.target.value);
   };
 
-  const submitNewNickName = () => {
-    // dispatch(updateNickName(newNickName));
-  };
+  useEffect(() => {
+    const checkIsDuplicated = async (name) => {
+      const response = await userApi.get(`/find?nickname=${newNickName}`);
+    
+      setIsDuplicated(response.data.data.isExistNickname);
+    };
 
-  const checkIsDuplicated = async (name) => {
-    const response = await userApi.get(`find?nickname=${newNickName}`);
-    setIsDuplicated(response.data.isExistNickname);
-  };
+    let intervalId;
+
+    if (isFocused) {
+      intervalId = setInterval(() => {
+
+        checkIsDuplicated(newNickName);
+      }, 500);
+    }
+
+    return () => {
+      if (intervalId) clearInterval(intervalId);
+    };
+  }, [isFocused, newNickName, isDuplicated]);
 
   return (
     <>
@@ -46,28 +68,33 @@ const ChangeNickName = () => {
             type="text"
             value={newNickName}
             onChange={handleNickNameChange}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
             placeholder={user.nickName}
             className="nickname-input"
           />
           {!isDuplicated && newNickName !== "" && (
             <div>
-              <p >사용 가능한 닉네임입니다.</p>
+              <p>사용 가능한 닉네임입니다.</p>
             </div>
           )}
           {newNickName === "" && (
             <div>
-              <p >닉네임을 입력해주세요</p>
+              <p>닉네임을 입력해주세요</p>
             </div>
           )}
           {isDuplicated && newNickName !== "" && (
             <div>
-              <p >사용 가능한 닉네임입니다.</p>
+              <p>사용 불가능한 닉네임입니다.</p>
             </div>
           )}
           <button
             className="nickname-confirm"
+            onClick={submitNewNickName}
             disabled={isDuplicated || newNickName === ""}
-          >확인</button>
+          >
+            확인
+          </button>
         </div>
         <div className="change-nick-name-footer">
           <MainNav />
