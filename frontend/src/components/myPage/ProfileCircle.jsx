280b5cff543df9f47022b1d1f5b1ec4f2e1bef2a
ayoung-kim <ayoung7777@gmail.com>
2024-08-02 01:02:22 +0900

    [FEAT][FE][아영] 파일 불러와서 화면 확인

--- frontend/src/components/myPage/ProfileCircle.jsx
+++ frontend/src/components/myPage/ProfileCircle.jsx
@@ -1,7 +1,52 @@
 import "./ProfileCircle.css";
+import React, { useRef, useState } from "react";
 
 const ProfileCircle = () => {
-  return <div className="profile-circle">동그라미</div>;
+  const [imageSrc, setImageSrc] = useState(null);
+  const fileInputRef = useRef(null);
+
+  const handleClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImageSrc(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  return (
+    <>
+      <div
+        className="profile-circle"
+        style={{
+          backgroundImage: imageSrc ? `url(${imageSrc})` : "none",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        {imageSrc ? "" : "동그라미"}
+      </div>
+      <img
+        src="/img/camera-logo.png"
+        alt="camera-logo"
+        onClick={handleClick}
+        style={{ cursor: "pointer", width: "5%" }}
+      />
+
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: "none" }}
+        onChange={handleFileChange}
+      />
+    </>
+  );
 };
 
 export default ProfileCircle;
