45df56e27b5c98b64a42bbc4712c393cf42b8355
ayoung-kim <ayoung7777@gmail.com>
2024-08-01 17:10:54 +0900

    [FEAT][FE][아영] 모달 창 생성

--- frontend/src/components/common/MainHeader.jsx
+++ frontend/src/components/common/MainHeader.jsx
@@ -2,15 +2,19 @@ import "./MainHeader.css";
 import { useNavigate } from "react-router-dom";
 
 // 3요소: <, 해당 페이지 이름, 해당 페이지 아이콘(없을 수도 있음)
-const MainHeader = ({ title, icon }) => {
+const MainHeader = ({ title, category, icon }) => {
   const nav = useNavigate();
   return (
-    <header className="header">
-      <div className="header_prev" onClick={() => nav(-1)}>
-        &lt;
+    <header className="header w-full h-full">
+      <div className="header_left">
+        <div className="header_prev" onClick={() => nav(-1)}>
+          &lt;
+        </div>
+        <div className="header_title">{title}</div>
+        {/* 조건부 렌더링, icon을 넣어주면 icon이 나오도록 설계 */}
+        {category && <div className="header_category bg-color2">category</div>}
       </div>
-      <div className="header_title">{title}</div>
-      {/* 조건부 렌더링, icon을 넣어주면 icon이 나오도록 설계 */}
+
       {icon && <div className="header_icon">{icon}</div>}
     </header>
   );
--- frontend/src/components/puzzleBoard/PuzzleCanvas.jsx
+++ frontend/src/components/puzzleBoard/PuzzleCanvas.jsx
@@ -1,21 +1,39 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import paper from "paper";
 import createTiles from "./createTiles";
-import puzzle3X4Config from "../../utils/puzzleBoard/puzzle3X4Config";
-import puzzle4X5Config from "../../utils/puzzleBoard/puzzle4X5Config";
 import fitTiles from "./fitTiles";
 import autoSnapTiles from "./autoSnapTiles";
+
+import puzzle3X4Config from "../../utils/puzzleBoard/puzzle3X4Config";
+import puzzle4X5Config from "../../utils/puzzleBoard/puzzle4X5Config";
 import puzzle5X6Config from "../../utils/puzzleBoard/puzzle5X6Config";
+
+import ChooseImg from "../modal/board/ChooseImg";
+import "./PuzzleCanvas.css";
 
 const PuzzleCanvas = () => {
   const canvasRef = useRef(null);
+
+  // 모달 창
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalData, setModalData] = useState(null);
+
+  const openModal = (data) => {
+    setModalData(data);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setModalData(null);
+  };
 
   useEffect(() => {
     // paper.js 초기화
     paper.setup(canvasRef.current);
 
     // 퍼즐 크기 지정
-    const level = 3;
+    const level = 2;
     let boardConfig;
     if (level == 1) boardConfig = puzzle3X4Config;
     else if (level == 2) boardConfig = puzzle4X5Config;
@@ -24,6 +42,7 @@ const PuzzleCanvas = () => {
 
     // 퍼즐 조각 생성
     const { tiles, tileIndexes } = createTiles(boardConfig);
+
     // 퍼즐 조각 배치
     fitTiles(tiles, boardConfig);
     //autoSnapTiles(tiles, boardConfig);
@@ -31,7 +50,11 @@ const PuzzleCanvas = () => {
     // 클릭 이벤트 생성
     tiles.forEach((tile) => {
       tile.onClick = (event) => {
-        console.log("onClickTile", tile.index);
+        const data = {
+          tile: tile,
+          tileWidth: boardConfig.tileWidth,
+        };
+        openModal(data, closeModal);
       };
     });
 
@@ -41,12 +64,9 @@ const PuzzleCanvas = () => {
   }, []);
 
   return (
-    <div>
-      <canvas
-        style={{ width: "300px", height: "300px" }}
-        ref={canvasRef}
-        className="canvas"
-      ></canvas>
+    <div className="canvas-container">
+      <canvas ref={canvasRef} className="canvas"></canvas>
+      {isModalOpen && <ChooseImg data={modalData} onClose={closeModal} />}
     </div>
   );
 };
