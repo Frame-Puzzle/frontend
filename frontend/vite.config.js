6e6d4f026f2d6b259aa4d140818cb955a06e85e6
ayoung-kim <ayoung7777@gmail.com>
2024-08-03 16:24:44 +0900

    [ADD][FE][아영] vite.config.js에 라이브러리 추가

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -24,6 +24,11 @@ export default defineConfig({
       // },
     }),
   ],
+  build: {
+    rollupOptions: {
+      external: ['browser-image-compression']
+    }
+  },
   server: {
     port: 3000,
     host: "0.0.0.0",
