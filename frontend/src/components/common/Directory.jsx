3e4d887520317af7d8c4497a1d324d5b8ec76faf
THIP-Y <nine14141732@gmail.com>
2024-08-05 10:48:28 +0900

    [FEAT][FE][승찬] 홈페이지에서 특정 디렉토리 페이지로 라우트 하는 기능 구현

--- frontend/src/components/common/Directory.jsx
+++ frontend/src/components/common/Directory.jsx
@@ -1,8 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import "./Directory.css";
 
 const Directory = (props) => {
+
+  let navigate = useNavigate();
+
   return (
-    <div className="directory">
+    <div className="directory" onClick={() => {
+      navigate(`/directories/${props.info.directoryId}`);
+    }}>
       <div className="directory-icon">
         <img src="https://frazzle208.s3.ap-northeast-2.amazonaws.com/img/directory-folder.png" alt="directory-icon"></img>
       </div>
