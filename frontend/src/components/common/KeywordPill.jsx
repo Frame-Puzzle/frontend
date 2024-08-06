55f13d1b5454c12b908b246fae7446894dcceb03
THIP-Y <nine14141732@gmail.com>
2024-08-07 00:22:08 +0900

    [DESIGN][FE][승찬] keyword 알약 컴포넌트 기본 디자인 완성

--- frontend/src/components/common/KeywordPill.jsx
+++ frontend/src/components/common/KeywordPill.jsx
@@ -2,7 +2,12 @@ import "./KeywordPill.css";
 
 const KeywordPill = (props) => {
   return (
-    <div>#{props.info}</div>
+    <div className="keyword-pill flex">
+      <span>#{props.info}</span>
+      <div className="x-icon-container">
+        <img src="https://frazzle208.s3.ap-northeast-2.amazonaws.com/img/x-symbol.png" alt="x-icon" />
+      </div>
+    </div>
   )
 }
 
