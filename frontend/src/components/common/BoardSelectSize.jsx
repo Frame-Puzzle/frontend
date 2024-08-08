e6d81f58944b395de02dc6b471aac70308949020
THIP-Y <nine14141732@gmail.com>
2024-08-08 17:38:32 +0900

    [DESIGN][FE][승찬] 퍼즐판 생성화면 1-1, 1-2 디자인 완료

--- frontend/src/components/common/BoardSelectSize.jsx
+++ frontend/src/components/common/BoardSelectSize.jsx
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
 import "./BoardSelectSize.css";
 import { setBoardSize } from "../../stores/createBoardSlice";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 // Create Board의 첫 번째 전환 컴포넌트
@@ -10,6 +10,26 @@ const BoardSelectSize = () => {
   let dispatch = useDispatch();
   let navigate = useNavigate();
   let [activate, setActivate] = useState(false);
+  
+  let [whatSize, setWhatSize] = useState(0);
+  let [twelve, setTwelve] = useState('');
+  let [twenty, setTwenty] = useState('');
+  let [thirty, setThirty] = useState('');
+
+  useEffect(() => {
+    if (whatSize === 12) {
+      setTwelve('checked');
+    } else if (whatSize === 20) {
+      setTwenty('checked');
+    } else if (whatSize === 30) {
+      setThirty('checked');
+    }
+    return () => {
+      setTwelve('');
+      setTwenty('');
+      setThirty('');
+    }
+  }, [whatSize])
 
   return (
     <div className="board-select-size w-full h-full">
@@ -21,21 +41,24 @@ const BoardSelectSize = () => {
         <span>퍼즐 판의 크기를</span>
         <span>설정해 주세요</span>
       </div>
-      <div className="board-select-size-button">
-        <span onClick={() => {
+      <div className="board-select-size-button flex">
+        <span id={twelve} onClick={() => {
           dispatch(setBoardSize(12));
           setActivate(true);
-        }}>3X4</span>
-        <span onClick={() => {
+          setWhatSize(12);
+        }}>3 X 4</span>
+        <span id={twenty} onClick={() => {
           dispatch(setBoardSize(20));
           setActivate(true);
-        }}>4X5</span>
-        <span onClick={() => {
+          setWhatSize(20);
+        }}>4 X 5</span>
+        <span id={thirty} onClick={() => {
           dispatch(setBoardSize(30));
           setActivate(true);
-        }}>5X6</span>
+          setWhatSize(30);
+        }}>5 X 6</span>
       </div>
-      <div className="board-select-size-next">
+      <div className="board-select-size-next flex">
         { activate ? <span id="board-select-size-next-activate" onClick={() => {
           navigate(`/create-board/mission-on`);
         }}>다음</span> : null }
