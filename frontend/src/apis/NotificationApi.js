9778bf5cb2fbb05b637216233ffb4205cf8df2df
hyun-young <jhy7547@naver.com>
2024-08-08 13:02:40 +0900

    [FIX][FE][현영] 알림 api 일부 수정

--- frontend/src/apis/NotificationApi.js
+++ frontend/src/apis/NotificationApi.js
@@ -12,7 +12,6 @@ NotificationApi.interceptors.request.use(
   (config) => {
     const state = store.getState();
     const accessToken = state.user.accessToken;
-
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
     }
@@ -24,26 +23,21 @@ NotificationApi.interceptors.request.use(
   }
 );
 
-// axios 객체에 응답 인터셉터 추가하기 (로그인 화면으로 보내기)
 NotificationApi.interceptors.response.use(
   (response) => response,
   (error) => {
-    //권한 오류 발생 시
     console.error("요청 응답 오류", error);
-    console.log("error", error.response.data);
-    const status = error.response.status;
+    if (error.response) {
+      console.log("error", error.response.data);
+      const status = error.response.status;
 
-    // 사용자 인증이 실패한 경우, 로그인 페이지로 리다이렉트
-    if (status === 401) {
-      store.dispatch(setAccessToken(""));
-      window.location.href = "/";
-    }
-    // 요청이 만들어졌지만 서버로부터 응답이 없을 때, error.request에 요청 정보가 들어간다.
-    else if (error.request) {
+      if (status === 401) {
+        store.dispatch(setAccessToken(""));
+        window.location.href = "/";
+      }
+    } else if (error.request) {
       console.error("No response received from the server:", error.request);
-    }
-    // 그 외의 에러 메시지를 처리
-    else {
+    } else {
       console.error("Error setting up request:", error.message);
     }
     return Promise.reject(error);
