6d2750da71b0bda95bfe2b1a5e3afb978552778a
홍경현 <hyeon21owo@gmail.com>
2024-08-09 23:23:28 +0900

    [DESIGN][FE][경현] 디렉토리 생성 모달창 에러 메세지 디자인

--- frontend/src/components/common/ExceptionMessage.jsx
+++ frontend/src/components/common/ExceptionMessage.jsx
@@ -6,7 +6,7 @@ const ExceptionMessage = (props) => {
   } else if (props.exceptionMessage === 1) {
     return (<span className="exception-message">올바르지 않은 형식입니다.</span>);
   } else if (props.exceptionMessage === 2) {
-    return (<span className="exception-message">길이가 너무 깁니다.</span>)
+    return (<span className="exception-message">한글 10자 또는 영문 20자까지 입력 가능합니다.</span>)
   } else {
     return (<span className="exception-message" style={{color: "#27C28A"}}>이 이름은 사용할 수 있습니다.</span>)
   }
