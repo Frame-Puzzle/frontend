c8d7683749ff15c3bbbb8bbed7541029174cc267
THIP-Y <nine14141732@gmail.com>
2024-08-10 23:34:37 +0900

    [DESIGN][FE][승찬] 이미지 업로드 모달창 예외 문구 포함 디자인 수정

--- frontend/src/components/common/ImgExceptionMessage.jsx
+++ frontend/src/components/common/ImgExceptionMessage.jsx
@@ -3,13 +3,13 @@ import "./ImgExceptionMessage.css";
 const ImgExceptionMessage = ({ exceptionMessage }) => {
 
   if (exceptionMessage === 0) {
-    return (<span>공백 포함 20자 이내로 작성해주세요.</span>);
+    return (<span className="img-exception-message">공백 포함 20자 이내로 작성해주세요.</span>);
   } else if (exceptionMessage === 1) {
-    return (<span>올바른 형식입니다.</span>)
+    return (<span className="img-exception-message" style={{color: "#27C28A"}}>올바른 형식입니다.</span>)
   } else {
-    return (<span>길이가 너무 깁니다.</span>)
+    return (<span className="img-exception-message">길이가 너무 깁니다.</span>)
   }
-  
+
 }
 
 export default ImgExceptionMessage;
