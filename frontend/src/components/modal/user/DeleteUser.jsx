61a68d17463f2e60f3cb5728653bdb252f554670
THIP-Y <nine14141732@gmail.com>
2024-08-12 15:54:05 +0900

    [FEAT][FE][승찬] 회원탈퇴 모달창 디자인 및 기능 이식 완료

--- frontend/src/components/modal/user/DeleteUser.jsx
+++ frontend/src/components/modal/user/DeleteUser.jsx
@@ -1,8 +1,34 @@
 import "./DeleteUser.css";
+import userApi from "../../../apis/userApi";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setAccessToken } from "../../../stores/userSlice";
 
-const DeleteUser = () => {
+const DeleteUser = ({ setModalFrame }) => {
+
+  let dispatch = useDispatch();
+  let navigate = useNavigate();
+
   return (
-    <div></div>
+    <div className="delete-user-modal">
+      <div className="delete-user-modal-body">
+        <span>정말 탈퇴하시겠습니까?</span>
+        <span>탈퇴하면 기존의 데이터가</span>
+        <span>모두 사라지며 복구되지 않습니다.</span>
+      </div>
+      <div className="delete-user-modal-footer flex">
+        <div className="delete-user-modal-button" onClick={() => { setModalFrame(false); }}>취소</div>
+        <div className="delete-user-modal-button" onClick={() => {
+          const asyncDelete = async () => {
+            const response = await userApi.delete();
+            dispatch(setAccessToken(""));
+            localStorage.clear();
+            navigate("/");
+          }
+          asyncDelete();
+        }}>탈퇴</div>
+      </div>
+    </div>
   )
 }
 
